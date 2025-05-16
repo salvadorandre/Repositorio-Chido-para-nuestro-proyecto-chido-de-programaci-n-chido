4d775bf90cd7d2310b5b48e84bb6f8d78f2ff1e0
SEdwinFBI <baquiaxfernando0@gmail.com>
2025-05-15 22:09:38 -0600

    git ignore

--- Program.cs
+++ Program.cs
@@ -29,11 +29,13 @@ builder.Services.AddControllers();
 var app = builder.Build();
 #region Middlewares
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("PoliticasCors");
